7f82045c9757425b20af92fcbbb191c388fd4254
ssoonan <ssoonan0770@gmail.com>
2022-06-26 12:30:53 +0900

    구현: 개인정보 수정 본인 것만 가능하게

--- src/main/java/next/web/UpdateUserServlet.java
+++ src/main/java/next/web/UpdateUserServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 import core.db.DataBase;
@@ -21,9 +22,25 @@ public class UpdateUserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        User user = DataBase.findUserById(req.getParameter("userId"));
-        log.debug("modify user: {}", user.getUserId());
-        req.setAttribute("user", user);
+
+        HttpSession session = req.getSession();
+        Object currentUserValue = session.getAttribute("user");
+
+        if (currentUserValue == null) {
+            resp.sendRedirect("/user/login.jsp");
+            return;
+        }
+
+        User currentUser= (User)currentUserValue;
+        User modifyUser = DataBase.findUserById(req.getParameter("userId"));
+
+        if (currentUser != modifyUser) {
+            resp.sendRedirect("/index.jsp");
+            return;
+        }
+
+        log.debug("modify user: {}", modifyUser.getUserId());
+        req.setAttribute("user", modifyUser);
         RequestDispatcher rd = req.getRequestDispatcher("/user/update.jsp");
         rd.forward(req, resp);
     }
