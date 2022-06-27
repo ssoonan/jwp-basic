0a9c253dfbd58a99b0554942d26e6d840c0be13f
ssoonan <ssoonan0770@gmail.com>
2022-06-27 12:46:27 +0900

    해결: build 시 classnotdeferror asd
    asd

--- src/main/java/next/WebServer.java
+++ src/main/java/next/WebServer.java
@@ -6,8 +6,8 @@ import org.apache.catalina.startup.Tomcat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class WebServerLauncher {
-    private static final Logger logger = LoggerFactory.getLogger(WebServerLauncher.class);
+public class WebServer {
+    private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
 
     public static void main(String[] args) throws Exception {
         String webappDirLocation = "webapp/";
